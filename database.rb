a592c42c0e894f9203cbadccf646b1fe88275b4b
Ian Johnson <ijohnson@covermymeds.com>
2019-09-14 09:56:33 -0400

    Improve the database api

--- database.rb
+++ database.rb
@@ -20,11 +20,9 @@ class Database
   #   exec_sql(sql)
   # end
 
-  def method_missing(name, *args)
+  def method_missing(name, params={})
     sql = @queries.fetch(name)
-    @pg_conn.exec_params(sql, args).to_a.map do |row|
-      Record.new(row)
-    end
+    Executor.new(@pg_conn, sql, params).execute
   end
 end
 
@@ -35,5 +33,33 @@ class Record
 
   def method_missing(col_name)
     @row.fetch(col_name.to_s)
+  end
+end
+
+class Executor
+  def initialize(pg_conn, sql, params)
+    @pg_conn = pg_conn
+    @sql = sql
+    @params = params
+  end
+
+  def execute
+    re = /{[a-zA-Z]\w*}/
+    var_names = @params.keys
+    args = @params.values
+
+    sql = @sql.gsub(re) do |encap_var_name|
+      var_name = encap_var_name.sub(/\A{/, '').sub(/}\z/, '').to_sym
+      index = var_names.index(var_name) + 1 # Because Postgres is 1-based
+      "$#{index}"
+    end
+
+    # puts '--'
+    # puts @sql
+    # puts sql
+
+    @pg_conn.exec_params(sql, args).to_a.map do |row|
+      Record.new(row)
+    end
   end
 end
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -4,29 +4,52 @@ describe Database do
   let(:db_url) { 'postgres://localhost/framework_dev' }
   let (:queries) do
     {
+      create: %{
+        create table submissions(name text, email text)
+      },
+      drop: %{
+        drop table if exists submissions
+      },
+      # create_submission: %{
+      #   insert into submissions(name, email)
+      #     values ($1, $2)
+      # },
       create_submission: %{
-        insert into submissions(name)
-          values ($1)
+        insert into submissions(name, email)
+          values ({name}, {email})
       },
       find_submission: %{
         select * from submissions
-          where name = $1
+          where name = {name}
       },
     }
   end
   let(:db) { Database.connect(db_url, queries) }
 
+  before do
+    db.drop
+    db.create
+  end
+
   it "does not have sql injection vulnerabilities" do
     name = "'; drop table submissions; --"
-    expect { db.find_submission(name) }
-      .to change { db.find_submission(name).length }
-      .by(0)
-      # .by(1)
+    email = 'alice@example.com'
+    expect { db.create_submission(name: name, email: email) }
+      .to change { db.find_submission(name: name).length }
+      .by(1)
   end
 
   it "retrieves records that it has inserted" do
-    db.create_submission('Alice')
-    alice = db.find_submission('Alice').fetch(0)
+    db.create_submission(name: 'Alice',
+                         email: 'alice@example.com')
+    alice = db.find_submission(name: 'Alice').fetch(0)
+    expect(alice.name).to eq 'Alice'
+  end
+
+  it "doesn't care about the order of params" do
+    db.create_submission(email: 'alice@example.com',
+                         name: 'Alice')
+    alice = db.find_submission(name: 'Alice').fetch(0)
     expect(alice.name).to eq 'Alice'
   end
 end
